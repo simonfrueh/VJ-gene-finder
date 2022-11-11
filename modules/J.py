d819acfcdc5a029beee342234195667850ff62b2
marfrh <jimmy_conway@gmx.de>
2022-11-11 15:47:56 +0100

    added separate moduel for helper functions

--- modules/J.py
+++ modules/J.py
@@ -1,83 +1,6 @@
 from Bio.Seq import Seq
 from Bio import motifs
-
-# return a list of all possible base sequences with wobble 
-# replaced
-def list_resolve_wobble_bases(seq_list):
-	
-	for i in range(0, len(seq_list)):
-		if seq_list[i].count("R") > 0 :
-			seq_list.append(seq_list[i].replace("R","A",1))
-			seq_list.append(seq_list[i].replace("R","G",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("Y") > 0 :
-			seq_list.append(seq_list[i].replace("Y","C",1))
-			seq_list.append(seq_list[i].replace("Y","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("M") > 0 :
-			seq_list.append(seq_list[i].replace("M","A",1))
-			seq_list.append(seq_list[i].replace("M","C",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("K") > 0 :
-			seq_list.append(seq_list[i].replace("K","G",1))
-			seq_list.append(seq_list[i].replace("K","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("S") > 0 :
-			seq_list.append(seq_list[i].replace("S","C",1))
-			seq_list.append(seq_list[i].replace("S","G",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("W") > 0 :
-			seq_list.append(seq_list[i].replace("W","A",1))
-			seq_list.append(seq_list[i].replace("W","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("B") > 0 :
-			seq_list.append(seq_list[i].replace("B","C",1))
-			seq_list.append(seq_list[i].replace("B","G",1))
-			seq_list.append(seq_list[i].replace("B","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("D") > 0 :
-			seq_list.append(seq_list[i].replace("D","A",1))
-			seq_list.append(seq_list[i].replace("D","G",1))
-			seq_list.append(seq_list[i].replace("D","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("H") > 0 :
-			seq_list.append(seq_list[i].replace("H","A",1))
-			seq_list.append(seq_list[i].replace("H","C",1))
-			seq_list.append(seq_list[i].replace("H","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("V") > 0 :
-			seq_list.append(seq_list[i].replace("V","A",1))
-			seq_list.append(seq_list[i].replace("V","C",1))
-			seq_list.append(seq_list[i].replace("V","G",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-			
-		if seq_list[i].count("N") > 0 :
-			seq_list.append(seq_list[i].replace("N","A",1))
-			seq_list.append(seq_list[i].replace("N","C",1))
-			seq_list.append(seq_list[i].replace("N","G",1))
-			seq_list.append(seq_list[i].replace("N","T",1))
-			seq_list.pop(i)
-			list_resolve_wobble_bases(seq_list)
-
+import modules.Functions as mF
 
 # check motif criteria for V gene
 # omega_aa: translated amino acid sequence of search region
@@ -176,15 +99,15 @@ def task_J(seq, rss, rc, result_list):
 	
 	# resolve wobble bases at highest task level to save time in loops
 	codon_list = ["GTRDGD"]
-	list_resolve_wobble_bases(codon_list)
+	mF.list_resolve_wobble_bases(codon_list)
 	
 	# resolve wobble bases at highest task level to save time in loops
 	z1 = ["TTNGGNNNNGG"]
-	list_resolve_wobble_bases(z1)
+	mF.list_resolve_wobble_bases(z1)
 	
 	# resolve wobble bases at highest task level to save time in loops
 	z2 = ["TNNNNNT"]
-	list_resolve_wobble_bases(z2)
+	mF.list_resolve_wobble_bases(z2)
 	
 	for r in rss:
 		
